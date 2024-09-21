00428c37bc4ea59a4610e1b2df4936dd3814ea55
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-09-21 09:24:48 -0400

    Add Header, Footer, Introsection

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
+import Intro from "@/components/ui/intro";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="py-24">
-      <div className="container">
-        <h1 className="text-3x1 font-bold">Portfolio</h1>
+      <div className="container max-w-3xl">
+        <Intro/>
 
       </div>
 
     </section>
   );
 }
-
-// Test test test
