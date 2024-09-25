234f02aec11c4db3cf836345165c95f78dbbe00a
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-09-24 21:28:54 -0400

    Adjust fading on scroll bar

--- components/header.tsx
+++ components/header.tsx
@@ -16,11 +16,11 @@ export default function Header() {
           <div className="absolute inset-y-0 left-0 z-10 w-1 bg-gradient-to-r from-background to-transparent pointer-events-none"></div>
           
 
-        <ul className='flex items-center gap-6 overflow-x-auto flex-nowrap text-sm font-light text-muted-foreground sm:gap-10 scroll-smooth whitespace-nowrap
+        <ul className='flex items-center gap-6 overflow-x-auto flex-nowrap text-sm font-light text-muted-foreground sm:gap-10 scroll-smooth whitespace-nowrap 
                          before:absolute before:left-0 before:top-0 before:bottom-0 before:w-8 before:bg-gradient-to-r before:from-background before:to-transparent
                          after:absolute after:right-0 after:top-0 after:bottom-0 after:w-8 after:bg-gradient-to-l after:from-background after:to-transparent
                          md:overflow-visible md:before:hidden md:after:hidden'>
-          <li className='transition-colors hover:text-foreground'>
+          <li className='transition-colors hover:text-foreground ml-5'>
             <Link href='/#intro'>About</Link>
           </li>
           <li className='transition-colors hover:text-foreground'>
@@ -29,7 +29,7 @@ export default function Header() {
           <li className='transition-colors hover:text-foreground'>
             <Link href='/#projects'>Work</Link>
           </li>
-          <li className='transition-colors hover:text-foreground'>
+          <li className='transition-colors hover:text-foreground mr-5'>
             <Link href='/#contact'>Contact</Link>
           </li>
 
